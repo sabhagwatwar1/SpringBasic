cb701d89debefac6a261dd51a1cb9829ba631957
Ram <sabhagwatwar@gmail.com>
2022-06-28 20:25:20 +0530

    Autowired xml byName

---
+++ SpringBasic/src/main/java/com/spring/autowire/Employee.java
@@ -0,0 +1,28 @@
+package com.spring.autowire;
+
+public class Employee {
+	private Address address;
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	public Employee(Address address) {
+		super();
+		this.address = address;
+	}
+
+	public Employee() {
+		super();
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [address=" + address + "]";
+	}
+	
+}
---
+++ SpringBasic/src/main/java/com/spring/autowire/Test.java
@@ -0,0 +1,12 @@
+package com.spring.autowire;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+	public static void main(String[] args) {
+		ApplicationContext context=new ClassPathXmlApplicationContext("com/spring/autowire/autowire.xml");
+		Employee emp=context.getBean("emp",Employee.class);
+		System.out.println(emp);
+	}
+}
